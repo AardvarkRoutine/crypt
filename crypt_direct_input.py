6cea43af5ac7fc146503ba2c95bcf83af8372cb5
AardvarkRoutine <52427190+AardvarkRoutine@users.noreply.github.com>
2024-03-19 13:23:34 +0100

    Works!

--- crypt_direct_input.py
+++ crypt_direct_input.py
@@ -1,30 +1,30 @@
 def crypt(string, shift):
-    str = ""
+    # Leerer String, um die verschlüsselte/entschlüsselte Nachricht zu speichern
+    result = ""
+    
+    # Schleife durch jeden Buchstaben in der Eingabezeichenkette
     for char in string:
-        if char.isalpha():
-            offset = ord('A') if char.isupper() else ord('a')
-            str += chr((ord(char) - offset + shift) % 26 + offset)
+        if char.isalpha():  # Überprüft, ob der aktuelle Zeichen ein Buchstabe ist
+            offset = ord('A') if char.isupper() else ord('a')  # Offset für Groß- und Kleinbuchstaben
+            result += chr((ord(char) - offset + shift) % 26 + offset)  # Verschlüsselung/Entschlüsselung unter Berücksichtigung des Offsets
         else:
-            str += char
-    return str
+            result += char  # Fügt Nicht-Buchstaben unverändert hinzu
+    return result
 
 
 while True:
-
+    # Eingabe der Nachricht, des Offsets und der Richtung (verschlüsseln oder entschlüsseln)
     prompt = str(input("Nachricht: "))
     prompt_offset = str(input("Offset: "))
     offset = 0
     prompt_direction = str(input("d for decrypt OR e for encrypt: "))
 
     if 'd' in prompt_direction:
-        offset = int(prompt_offset)
+        offset = int(prompt_offset)  # Offset für Entschlüsselung
     elif 'e' in prompt_direction:
-        offset = -abs(int(prompt_offset))
+        offset = -abs(int(prompt_offset))  # Offset für Verschlüsselung
     else:
-        print("You fucked up!")
+        print("You fucked up!")  # Ausgabe, wenn die Eingabe ungültig ist
         exit()
 
-    print(crypt(prompt, offset))
-
-
-    
+    print(crypt(prompt, offset))  # Aufruf der Funktion zur Verschlüsselung/Entschlüsselung und Ausgabe des Ergebnisses
